96d87504fa39d62003fc2dc690d2215a51c769ad
ivanwe2 <45885771+ivanwe2@users.noreply.github.com>
2024-07-23 12:31:07 +0300

    feat: add create product endpoint using carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,29 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFilem, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //business logic to create product
-            throw new NotImplementedException();
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFilem,
+                Price = command.Price
+            };
+
+            //TODO
+            //save to db
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services
+builder.Services.AddCarter()
+                .AddMediatR(config =>
+                {
+                    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+                });
 
 var app = builder.Build();
 
 //Configure HTTPS pipeline
+app.MapCarter();
 
 app.Run();
