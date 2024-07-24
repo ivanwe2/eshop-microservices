36b61124ecf4eab4656a42a888703848165eccaa
Ivan Rusev <ivanrusev03@schoolmath.eu>
2024-07-24 15:49:45 +0300

    feat: seed marten db with IInitialData

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -16,6 +16,11 @@ builder.Services
         options.Connection(builder.Configuration.GetConnectionString("Database")!);
     }).UseLightweightSessions();
 
+if(builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
