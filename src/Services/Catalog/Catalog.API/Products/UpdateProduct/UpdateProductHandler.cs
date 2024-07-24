03bb0618241118077ffc971349a544ba3b1a9b91
Ivan Rusev <ivanrusev03@schoolmath.eu>
2024-07-24 14:07:03 +0300

    chore: add put and delete endpoint validators

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,4 +1,5 @@
 ﻿
+using Catalog.API.Products.CreateProduct;
 using Catalog.API.Products.GetProductById;
 
 namespace Catalog.API.Products.UpdateProduct
@@ -6,6 +7,21 @@ namespace Catalog.API.Products.UpdateProduct
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required!");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required!")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters!");
+
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0!");
+        }
+    }
+
     internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
