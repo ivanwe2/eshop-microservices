b4ad66e66cd17bd15f1a99ab31342b226d8dfb5e
Ivan Rusev <ivanrusev03@schoolmath.eu>
2024-07-25 12:41:37 +0300

    feat: decorate repository using redis cache
    decorator pattern using Scrutor library, proxy pattern for the CachedBasketRepo and decorator pattern

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using Basket.API.Data;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,22 @@ builder.Services
         options.Schema.For<ShoppingCart>().Identity(x=>x.UserName);
     }).ApplyAllDatabaseChangesOnStartup().UseLightweightSessions();
 
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//Best Practice to decorate
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
+
+//Second way
+//builder.Services.AddScoped<IBasketRepository>(provider =>
+//{
+//    var basketRepository = provider.GetService<IBasketRepository>();
+//    return new CachedBasketRepository(basketRepository!, provider.GetRequiredService<IDistributedCache>());
+//});
 
 //if (builder.Environment.IsDevelopment())
 //{
