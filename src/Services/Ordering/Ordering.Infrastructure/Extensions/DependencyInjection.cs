021c66714b858def2ec3974c216565b509149848
ivanwe2 <45885771+ivanwe2@users.noreply.github.com>
2024-07-26 15:28:54 +0300

    feat: add entity type configuration + complex objs for value objs

--- src/Services/Ordering/Ordering.Infrastructure/Extensions/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Extensions/DependencyInjection.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 
 namespace Ordering.Infrastructure.Extensions
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Database");
 
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped<ApplicationDbContext>();
             return services;
         }
     }
