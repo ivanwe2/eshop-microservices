ef9de6cfb2156718ba187ea4eb345080e33cfaf3
ivanwe2 <45885771+ivanwe2@users.noreply.github.com>
2024-07-26 10:54:59 +0300

    chore: adhere to clean architecture, separate projects, add DI
    add domain abstractions - entity aggregates and domainevents

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,20 @@
+using Ordering.API.Extensions;
+using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 var app = builder.Build();
 
 //Config http request pipeline
+
+app.UseApiServices();
 
 app.Run();
