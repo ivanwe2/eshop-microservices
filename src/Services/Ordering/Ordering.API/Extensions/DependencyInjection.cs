554f1593385803ae8ede13551b30ddb48233c04b
Ivan Rusev <ivanrusev03@schoolmath.eu>
2024-07-29 12:52:32 +0300

    feat: add health checks for api and db

--- src/Services/Ordering/Ordering.API/Extensions/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/Extensions/DependencyInjection.cs
@@ -1,14 +1,18 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
 
 namespace Ordering.API.Extensions
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
 
             services.AddExceptionHandler<CustomExceptionHandler>();
+
+            services.AddHealthChecks()
+                    .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -18,6 +22,12 @@ namespace Ordering.API.Extensions
             app.MapCarter();
 
             app.UseExceptionHandler(options => { });
+
+            app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+                
 
             return app;
         }
